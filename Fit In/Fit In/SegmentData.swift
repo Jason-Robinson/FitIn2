689b84a55811c990151f4d9e8cc1a53205c0fb2f
Jason Robinson <Jason@mwc-225040.dhcp.missouri.edu>
2014-11-24 21:14:18 -0600

    pre branch separation

---
+++ Fit In/Fit In/SegmentData.swift
@@ -0,0 +1,16 @@
+//
+//  SegmentData.swift
+//  Fit In
+//
+//  Created by Jason Robinson on 11/24/14.
+//  Copyright (c) 2014 Lexus LLC. All rights reserved.
+//
+
+import UIKit
+
+class SegmentData: NSObject {
+   
+    var gym: [String] = ["gym1",  "gym"]
+    var home: [String] = ["home", "home1"]
+    var office: [String] = ["office", "office1"]
+}
